4a688375fbb5803e3370d8d2b2c92ec27203dd5b
Gleb <114811823+NovaCript@users.noreply.github.com>
2024-03-07 14:10:18 +0300

    Update tasks.py

--- src/tasks.py
+++ src/tasks.py
@@ -1,15 +1,20 @@
 from celery import Celery
-vasya = Celery('tasks', broker='redis://localhost:6379/0',backend='redis://localhost:6379/0')
-vasya.config_from_object('celeryconfig')
+celery = Celery('tasks', broker='redis://localhost:6379/0',backend='redis://localhost:6379/0')
+celery.config_from_object('celeryconfig')
 
-@vasya.task
+@celery.task
 def add(x,y):
     return x+y
 
 
 
 async def get_task_result(task_id: str):
-    task = AsyncResult(task_id, app=vasya)
+    task = AsyncResult(task_id, app=celery)
     if task.state == "SUCCESS":
         return {'task_id': task_id, 'status': task.state, 'result': await task.get()}
     return {'task_id': task_id, 'status': task.state}
+
+
+async def send_task(x:int, y:int):
+    task = celery.send_task('tasks.add', args=[x, y])
+    return {'task_id': str(task.id)}
