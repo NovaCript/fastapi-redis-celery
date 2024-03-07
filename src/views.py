44e8e20e105033e5ea1522619aece8348754db93
Gleb <114811823+NovaCript@users.noreply.github.com>
2024-03-07 14:10:28 +0300

    Update views.py

--- src/views.py
+++ src/views.py
@@ -1,17 +1,12 @@
 from fastapi import APIRouter
-from tasks import vasya
-from celery.result import AsyncResult
+from tasks import get_task_result, send_task
 
 router = APIRouter()
 
 @router.post('/add/')
 async def add_task(x: int, y: int):
-    task = vasya.send_task('tasks.add', args=[x, y])
-    return {'task_id': str(task.id)}
+    return await send_task(x,y)
 
-@router.get('/tasks/{task_id}')
-async def get_task_result(task_id: str):
-    task = AsyncResult(task_id, app=vasya)
-    if task.state == "SUCCESS":
-        return {'task_id': task_id, 'status': task.state, 'result': task.result}
-    return {'task_id': task_id, 'status': task.state}
+@app.get('/tasks/{task_id}')
+async def get_task(task_id: str):
+    return await get_task_result(task_id)
