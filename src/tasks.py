61434fdee6eb49e875d1f3e0315ab682761df2cc
Gleb <114811823+NovaCript@users.noreply.github.com>
2024-03-07 14:04:09 +0300

    Update tasks.py

--- src/tasks.py
+++ src/tasks.py
@@ -5,3 +5,11 @@ vasya.config_from_object('celeryconfig')
 @vasya.task
 def add(x,y):
     return x+y
+
+
+
+async def get_task_result(task_id: str):
+    task = AsyncResult(task_id, app=vasya)
+    if task.state == "SUCCESS":
+        return {'task_id': task_id, 'status': task.state, 'result': await task.get()}
+    return {'task_id': task_id, 'status': task.state}
